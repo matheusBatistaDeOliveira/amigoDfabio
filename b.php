ceba82e0a20c103393437cd2d2ad40126ffc3d33
Matheusz <matheussetacursos@gmail.com>
2026-03-03 21:41:04 -0300

    addFile_b

---
+++ b.php
@@ -0,0 +1,11 @@
+<?php 
+	$num1=10; //Int
+	$num2=10.2; //Decimal
+	$nome="Etecia"; //String
+	$sobreNome=' Agostina'; //String
+ 	$status= true; //Boolean
+ 	$flag=false; //Boolean
+ 
+ 	$resp = $num1 % $num2;
+ 	$resp2 = $nome + $sobreNome;
+ ?>
