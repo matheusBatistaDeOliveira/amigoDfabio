a6fc9b2431483821703affb94c39922851bacb6e
Matheuszin <matheussetacursos@gmail.com>
2026-04-28 22:19:46 -0300

    POST e FORM AULA CHEIA

---
+++ aulaQuatroContact/phpContact/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<title></title>
+</head>
+<body>
+
+<h1>tituloEnorme</h1>
+
+<script src="./script.js"></script>
+</body>
+</html>
+
+<?php 
+	$uva;
+	echo "oi";
+ ?>
---
+++ aulaQuatroContact/phpContact/script.js
@@ -0,0 +1,3 @@
+let n1 = prompt("Digite o primeiro número");
+
+let n2 = prompt("Digite o segundo número");
